fee5a98622877354fd1f01d802dba305a1f7632b
Dylan Smith <Dylan.Smith@shopify.com>
2012-02-14 12:38:15 -0500

    [feature] Add support for customer search and customer group search.

--- lib/shopify/resources.py
+++ lib/shopify/resources.py
@@ -202,11 +202,32 @@ class Event(ShopifyResource):
 
 
 class Customer(ShopifyResource):
-    pass
+    @classmethod
+    def search(cls, **kwargs):
+        """Search for customers matching supplied query
+
+        Args:
+           q: Text to search for customers ("q" is short for query)
+           f: Filters to apply to customers ("f" is short for query)
+           page: Page to show (default: 1)
+           limit: Maximum number of results to show (default: 50, maximum: 250)
+        Returns:
+           An array of customers.
+        """
+        return cls._build_list(cls.get("search", **kwargs))
 
 
 class CustomerGroup(ShopifyResource):
-    pass
+    def customers(cls, **kwargs):
+        """Get a list of customers matching a customer group
+
+        Args:
+           page: Page to show (default: 1)
+           limit: Maximum number of results to show (default: 50, maximum: 250)
+        Returns:
+           An array of customers.
+        """
+        return Customer._build_list(cls.get("customers", **kwargs))
 
 
 class Theme(ShopifyResource):
