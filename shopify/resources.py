6ee7a304ef7719513429e903303be262b21ec39e
Dylan Smith <Dylan.Smith@shopify.com>
2012-11-29 09:49:42 -0500

    Fix bug in setting format attribute on Webhook instances.
    Previously the class method was getting set rather than the attribute.

--- shopify/resources.py
+++ shopify/resources.py
@@ -190,7 +190,13 @@ class Redirect(ShopifyResource):
 
 
 class Webhook(ShopifyResource):
-    pass
+    def __get_format(self):
+        return self.attributes.get("format")
+
+    def __set_format(self, data):
+        self.attributes["format"] = data
+
+    format = property(__get_format, __set_format, None, "Format attribute")
 
 
 class Event(ShopifyResource):
