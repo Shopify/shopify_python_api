1a07804f1521bedd42b48c95c6147d63c239f863
Kevin Hughes <kevin.hughes@shopify.com>
2014-01-15 15:22:45 +0000

    fixes for travis ci testing

--- test/order_test.py
+++ test/order_test.py
@@ -4,8 +4,6 @@ from pyactiveresource.activeresource import ActiveResource
 from pyactiveresource.util import xml_to_dict
 
 class OrderTest(TestCase):
-    def setUp(self):
-        ActiveResource.site = "http://localhost"
 
     def test_should_be_loaded_correctly_from_order_xml(self):
         order_xml = """<?xml version="1.0" encoding="UTF-8"?>
@@ -24,16 +22,16 @@ class OrderTest(TestCase):
         note_attribute = order.note_attributes[0]
         self.assertEqual("size", note_attribute.name)
         self.assertEqual("large", note_attribute.value)
-      
+
     def test_should_be_able_to_add_note_attributes_to_an_order(self):
         order = shopify.Order()
         order.note_attributes = []
         order.note_attributes.append(shopify.NoteAttribute({'name': "color", 'value': "blue"}))
-        
+
         order_xml = xml_to_dict(order.to_xml())
         note_attributes = order_xml["order"]["note_attributes"]
         self.assertTrue(isinstance(note_attributes, list))
-        
+
         attribute = note_attributes[0]
         self.assertEqual("color", attribute["name"])
         self.assertEqual("blue", attribute["value"])
