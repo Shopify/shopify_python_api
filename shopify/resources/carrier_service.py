d8a2b35c839960c18e4790c794750c380d09f6c6
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-07-17 14:23:47 -0400

    Add format setter and getter to CarrierService resource.
    Otherwise python will fallback on the class variable that pyactiveresource
    defines.

--- shopify/resources/carrier_service.py
+++ shopify/resources/carrier_service.py
@@ -2,4 +2,10 @@ from ..base import ShopifyResource
 
 
 class CarrierService(ShopifyResource):
-    pass
+    def __get_format(self):
+        return self.attributes.get("format")
+
+    def __set_format(self, data):
+        self.attributes["format"] = data
+
+    format = property(__get_format, __set_format, None, "Format attribute")
