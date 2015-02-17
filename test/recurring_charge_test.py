43a1af9a125f81ec58656a71c2d176eea8432de4
Arsi Halttunen <arsi@giosg.com>
2015-02-17 10:07:00 +0200

    Fixed RecurringApplicationChrage resources current() method to work correctly and added tests. See issue https://github.com/Shopify/shopify_python_api/issues/85

--- test/recurring_charge_test.py
+++ test/recurring_charge_test.py
@@ -7,3 +7,17 @@ class RecurringApplicationChargeTest(TestCase):
         self.fake("recurring_application_charges/35463/activate", method='POST',headers={'Content-length':'0', 'Content-type': 'application/json'}, body=" ")
         charge = shopify.RecurringApplicationCharge({'id': 35463})
         charge.activate()
+
+    def test_current_method_returns_active_charge(self):
+        # Test that current() class method correctly returns
+        # first RecurringApplicationCharge with active status
+        self.fake("recurring_application_charges")
+        charge = shopify.RecurringApplicationCharge.current()
+        self.assertEqual(charge.id, 455696195)
+
+    def test_current_method_returns_none_if_active_not_found(self):
+        # Test that current() class method correctly returns
+        # None if RecurringApplicationCharge with active status not found
+        self.fake("recurring_application_charges", body=self.load_fixture("recurring_application_charges_no_active"))
+        charge = shopify.RecurringApplicationCharge.current()
+        self.assertEqual(charge, None)
