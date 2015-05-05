ad40dca9ee46cc5f8d11ba865b7e28f2da959822
Gavin Ballard <gavin@gavinballard.com>
2015-05-05 12:29:50 +0200

    Add failing test for unicode parameters to validate_hmac

--- test/session_test.py
+++ test/session_test.py
@@ -125,6 +125,18 @@ class SessionTest(TestCase):
         }
         self.assertEqual(shopify.Session.calculate_hmac(params), params['hmac'])
 
+    def test_hmac_validation(self):
+        # Test using the secret and parameter examples given in the Shopify API documentation.
+        shopify.Session.secret='hush'
+        params = {
+          'shop': 'some-shop.myshopify.com',
+          'code': 'a94a110d86d2452eb3e2af4cfb8a3828',
+          'timestamp': '1337178173',
+          'signature': '6e39a2ea9e497af6cb806720da1f1bf3',
+          'hmac': unicode('2cb1a277650a659f1b11e92a4a64275b128e037f2c3390e3c8fd2d8721dac9e2'),
+        }
+        self.assertTrue(shopify.Session.validate_hmac(params))
+
     def test_return_token_if_hmac_is_valid(self):
         shopify.Session.secret='secret'
         params = {'code': 'any-code', 'timestamp': time.time()}
