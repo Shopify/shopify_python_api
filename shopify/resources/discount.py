bbdf00349f9c577f7835e61faa3ce039a71d7c2d
Daniel Smith <daniel@dastmalchi.com>
2015-09-29 11:06:14 -0700

    Adds remaining methods to Discount resource

--- shopify/resources/discount.py
+++ shopify/resources/discount.py
@@ -2,4 +2,9 @@ from ..base import ShopifyResource
 
 
 class Discount(ShopifyResource):
-    pass
+
+    def disable(self):
+        self._load_attributes_from_response(self.post("disable"))
+
+    def enable(self):
+        self._load_attributes_from_response(self.post("enable"))
