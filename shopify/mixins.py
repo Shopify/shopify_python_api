171f8a1d11bf20d685aa231515f0bc24553ec2bd
Julien Heller <fluxbooks@gmail.com>
2016-04-18 00:11:28 -0400

    Enabled accessing count on metafields

--- shopify/mixins.py
+++ shopify/mixins.py
@@ -16,6 +16,11 @@ class Metafields(object):
             _options = kwargs
         return shopify.resources.Metafield.find(resource=self.__class__.plural, resource_id=self.id, **_options)
 
+    def metafields_count(self, _options=None, **kwargs):
+        if _options is None:
+            _options = kwargs
+        return int(self.get("metafields/count", **_options))
+
     def add_metafield(self, metafield):
         if self.is_new():
             raise ValueError("You can only add metafields to a resource that has been saved")
