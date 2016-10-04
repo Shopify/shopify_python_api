42eddd2694d598addaa6799d13d5f0d334d1d296
Christopher Wrinn <cwrinn@paladinlogic.com>
2016-10-04 13:53:44 -0500

    Ported Limits from ruby api.

---
+++ shopify/limits.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+############################################################
+#
+# Copyright 2016 Paladin Logic, Ltd
+#           All Rights Reserved
+#
+############################################################
+
+
+# Imports
+from shopify import ShopifyResource
+
+CREDIT_LIMIT_HEADER_PARAM = 'x-shopify-shop-api-call-limit'
+
+# Functions
+def api_credit_limit_param():
+    return ShopifyResource.connection.response.headers[CREDIT_LIMIT_HEADER_PARAM].split('/')
+
+def credit_left():
+    return credit_limit() - credit_used()
+
+available_calls = credit_left
+
+def credit_maxed():
+    return credit_left() <= 0
+
+maxed = credit_maxed
+
+def credit_limit():
+    return api_credit_limit_param()[1]
+
+call_limit = credit_limit
+
+def credit_used():
+    return api_credit_limit_param()[0]
+
+call_count = credit_used
