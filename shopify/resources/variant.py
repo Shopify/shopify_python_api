4979e9de7deaafb41cd289acec5d118c6a8f242f
Christopher Wrinn <cwrinn@paladinlogic.com>
2016-10-13 18:16:14 -0500

    Modifications to get correct paths.

--- shopify/resources/variant.py
+++ shopify/resources/variant.py
@@ -8,7 +8,7 @@ class Variant(ShopifyResource, mixins.Metafields):
     @classmethod
     def _prefix(cls, options={}):
         product_id = options.get("product_id")
-        if product_id:
+        if options.get('is_new'):
             return "/admin/products/%s" % (product_id)
         else:
             return "/admin"
@@ -16,6 +16,8 @@ class Variant(ShopifyResource, mixins.Metafields):
     def save(self):
         if 'product_id' not in self._prefix_options:
             self._prefix_options['product_id'] = self.product_id
+        self._prefix_options['is_new'] = self.is_new()
+        
         return super(ShopifyResource, self).save()
 
     def add_image(self, image):
