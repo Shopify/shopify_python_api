d67d5a957589c990976d903e2ac53fd2c9e28b9b
Christopher Wrinn <cwrinn@paladinlogic.com>
2016-10-19 17:18:01 -0500

    Wasn't seeing prefix_option order_id

--- shopify/resources/fulfillment.py
+++ shopify/resources/fulfillment.py
@@ -4,6 +4,12 @@ from ..base import ShopifyResource
 class Fulfillment(ShopifyResource):
     _prefix_source = "/admin/orders/$order_id/"
 
+    def save(self):
+        if 'order_id' not in self._prefix_options:
+            self._prefix_options['order_id'] = self.order_id
+        
+        return super(ShopifyResource, self).save()
+
     def cancel(self):
         self._load_attributes_from_response(self.post("cancel"))
 
