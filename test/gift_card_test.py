7387d30e7cbec4ee773d939ffa0c739a1b8435c9
Sharoon Thomas <sharoon.thomas@fulfil.io>
2018-04-02 17:36:26 -0400

    Implement GiftCardAdjustment API

--- test/gift_card_test.py
+++ test/gift_card_test.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import shopify
 from test.test_helper import TestCase
 
@@ -21,3 +22,14 @@ class GiftCardTest(TestCase):
         self.assertFalse(gift_card.disabled_at)
         gift_card.disable()
         self.assertTrue(gift_card.disabled_at)
+
+    def test_adjust_gift_card(self):
+        self.fake('gift_cards/4208208', method='GET', code=200, body=self.load_fixture('gift_card'))
+        self.fake('gift_cards/4208208/adjustments', method='POST', code=201, body=self.load_fixture('gift_card_adjustment'), headers={'Content-type': 'application/json'})
+        gift_card = shopify.GiftCard.find(4208208)
+        self.assertEqual(gift_card.balance, "25.00")
+        adjustment = gift_card.add_adjustment(shopify.GiftCardAdjustment({
+            'amount': 100,
+        }))
+        self.assertIsInstance(adjustment, shopify.GiftCardAdjustment)
+        self.assertEqual(Decimal(adjustment.amount), Decimal("100"))
