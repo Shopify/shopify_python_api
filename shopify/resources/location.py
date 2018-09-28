04bfaae6c49fe8764b7802105502d70cc77d0892
Jamie Dwyer <jamie.dwyer@shopify.com>
2018-09-27 21:44:31 -0400

    Add inventory_levels method for Location resource

--- shopify/resources/location.py
+++ shopify/resources/location.py
@@ -1,5 +1,6 @@
 from ..base import ShopifyResource
-
+from .inventory_level import InventoryLevel
 
 class Location(ShopifyResource):
-    pass
+    def inventory_levels(self, **kwargs):
+        return InventoryLevel.find(from_="/admin/locations/%s/inventory_levels.json" % self.id, **kwargs)
