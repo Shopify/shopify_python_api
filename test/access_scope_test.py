c1c8eae0f21724fc5a0e6e5ba5ee7d0c5153de69
Jamie Dwyer <jamie.dwyer@shopify.com>
2018-10-09 16:32:09 -0400

    Add prefix source to AccessScope resource

--- test/access_scope_test.py
+++ test/access_scope_test.py
@@ -3,8 +3,8 @@ from test.test_helper import TestCase
 
 class AccessScopeTest(TestCase):
 
-  def test_all_should_return_all_checkouts(self):
-    self.fake('access_scopes')
+  def test_find_should_return_all_access_scopes(self):
+    self.fake('oauth/access_scopes', body=self.load_fixture('access_scopes'))
     scopes = shopify.AccessScope.find()
     self.assertEqual(3, len(scopes))
     self.assertEqual('read_products', scopes[0].handle)
