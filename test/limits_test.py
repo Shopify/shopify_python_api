cb5c4afc267f7225119552dc8228863b3faeed75
Tyler Ball <tyler@tylerball.net>
2019-04-04 17:02:01 -0400

    update limits.py and test

--- test/limits_test.py
+++ test/limits_test.py
@@ -2,39 +2,71 @@ import shopify
 from mock import patch
 from test.test_helper import TestCase
 
+
 class LimitsTest(TestCase):
+    """
+    API Calls Limit Tests
+
+    Conversion of test/limits_test.rb
+    """
+    @classmethod
+    def setUpClass(self):
+        self.original_headers = None
+
     def setUp(self):
         super(LimitsTest, self).setUp()
-
-        mock = patch('shopify.ShopifyResource.connection.response.get', side_effect=self.mock_limit_headers)
-        mock.start()
-
         self.fake('shop')
         shopify.Shop.current()
+        # TODO: Fake not support Headers
+        self.original_headers = shopify.Shop.connection.response.headers
 
     def tearDown(self):
         super(LimitsTest, self).tearDown()
-        patch.stopall()
+        shopify.Shop.connection.response.headers = self.original_headers
+
+    def test_raise_error_no_header(self):
+        with self.assertRaises(Exception):
+            shopify.Limits.credit_left()
+
+    def test_raise_error_invalid_header(self):
+        with patch.dict(
+                    shopify.Shop.connection.response.headers,
+                    {'bad': 'value'},
+                    clear=True):
+            with self.assertRaises(Exception):
+                shopify.Limits.credit_left()
 
     def test_fetch_limits_total(self):
-        self.assertEqual(40, shopify.Limits.credit_limit())
+        with patch.dict(
+                    shopify.Shop.connection.response.headers,
+                    {'X-Shopify-Shop-Api-Call-Limit': '40/40'},
+                    clear=True):
+            self.assertEqual(40, shopify.Limits.credit_limit())
 
     def test_fetch_used_calls(self):
-        self.assertEqual(1, shopify.Limits.credit_used())
+        with patch.dict(
+                    shopify.Shop.connection.response.headers,
+                    {'X-Shopify-Shop-Api-Call-Limit': '1/40'},
+                    clear=True):
+            self.assertEqual(1, shopify.Limits.credit_used())
 
     def test_calculate_remaining_calls(self):
-        self.assertEqual(39, shopify.Limits.credit_left())
+        with patch.dict(
+                    shopify.Shop.connection.response.headers,
+                    {'X-Shopify-Shop-Api-Call-Limit': '292/300'},
+                    clear=True):
+            self.assertEqual(8, shopify.Limits.credit_left())
 
-    def test_maxed_returns_true_when_credit_maxed_out(self):
-        self.assertFalse(shopify.Limits.is_credit_maxed())
+    def test_maxed_credits_false(self):
+        with patch.dict(
+                    shopify.Shop.connection.response.headers,
+                    {'X-Shopify-Shop-Api-Call-Limit': '125/300'},
+                    clear=True):
+            self.assertFalse(shopify.Limits.credit_maxed())
 
-        with patch('shopify.ShopifyResource.connection.response.get', side_effect=self.mock_limit_headers_max) as mock:
-            self.assertTrue(shopify.Limits.is_credit_maxed())
-
-    def mock_limit_headers(self, key):
-        if key == shopify.Limits.CREDIT_LIMIT_HEADER_PARAM:
-            return '1/40'
-
-    def mock_limit_headers_max(self, key):
-        if key == shopify.Limits.CREDIT_LIMIT_HEADER_PARAM:
-            return '40/40'
+    def test_maxed_credits_true(self):
+        with patch.dict(
+                    shopify.Shop.connection.response.headers,
+                    {'X-Shopify-Shop-Api-Call-Limit': '40/40'},
+                    clear=True):
+            self.assertTrue(shopify.Limits.credit_maxed())
