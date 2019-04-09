50031635f33306b9152fb956734d315e4ee4c7ce
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 10:23:35 -0700

    Fixed PYTHON_E302 issues in shopify/resources/location.py

--- shopify/resources/location.py
+++ shopify/resources/location.py
@@ -1,6 +1,7 @@
 from ..base import ShopifyResource
 from .inventory_level import InventoryLevel
 
+
 class Location(ShopifyResource):
     def inventory_levels(self, **kwargs):
         return InventoryLevel.find(from_="%s/locations/%s/inventory_levels.json" % (
