84e5746e066aaa5615fae68e41fed1dc7f57d408
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 10:19:58 -0700

    Fixed PYTHON_E251 issues in shopify/resources/price_rule.py

--- shopify/resources/price_rule.py
+++ shopify/resources/price_rule.py
@@ -4,14 +4,14 @@ from .discount_code import DiscountCode
 from .discount_code_creation import DiscountCodeCreation
 
 class PriceRule(ShopifyResource):
-    def add_discount_code(self, discount_code = DiscountCode()):
+    def add_discount_code(self, discount_code=DiscountCode()):
         resource = self.post("discount_codes", discount_code.encode())
         return DiscountCode(PriceRule.format.decode(resource.body))
 
     def discount_codes(self):
         return DiscountCode.find(price_rule_id=self.id)
 
-    def create_batch(self, codes = []):
+    def create_batch(self, codes=[]):
         codes_json = json.dumps({ 'discount_codes': codes})
 
         response = self.post("batch", codes_json.encode())
