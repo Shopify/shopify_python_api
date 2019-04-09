7fa0d72fba097ba5080e6d4bf43390e21a6a72c3
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 10:19:53 -0700

    Fixed PYTHON_E251 issues in test/order_risk_test.py

--- test/order_risk_test.py
+++ test/order_risk_test.py
@@ -4,7 +4,7 @@ from test.test_helper import TestCase
 class OrderRiskTest(TestCase):
 
   def test_create_order_risk(self):
-    self.fake("orders/450789469/risks", method='POST', body= self.load_fixture('order_risk'), headers={'Content-type': 'application/json'})
+    self.fake("orders/450789469/risks", method='POST', body=self.load_fixture('order_risk'), headers={'Content-type': 'application/json'})
     v = shopify.OrderRisk({'order_id':450789469})
     v.message = "This order was placed from a proxy IP"
     v.recommendation = "cancel"
@@ -18,24 +18,24 @@ class OrderRiskTest(TestCase):
     self.assertEqual(284138680, v.id)
 
   def test_get_order_risks(self):
-    self.fake("orders/450789469/risks", method='GET', body= self.load_fixture('order_risks'))
+    self.fake("orders/450789469/risks", method='GET', body=self.load_fixture('order_risks'))
     v = shopify.OrderRisk.find(order_id=450789469)
     self.assertEqual(2, len(v))
 
   def test_get_order_risk(self):
-    self.fake("orders/450789469/risks/284138680", method='GET', body= self.load_fixture('order_risk'))
+    self.fake("orders/450789469/risks/284138680", method='GET', body=self.load_fixture('order_risk'))
     v = shopify.OrderRisk.find(284138680, order_id=450789469)
     self.assertEqual(284138680, v.id)
 
   def test_delete_order_risk(self):
-    self.fake("orders/450789469/risks/284138680", method='GET', body= self.load_fixture('order_risk'))
+    self.fake("orders/450789469/risks/284138680", method='GET', body=self.load_fixture('order_risk'))
     self.fake("orders/450789469/risks/284138680", method='DELETE', body="destroyed")
     v = shopify.OrderRisk.find(284138680, order_id=450789469)
     v.destroy()
 
   def test_delete_order_risk(self):
-    self.fake("orders/450789469/risks/284138680", method='GET', body= self.load_fixture('order_risk'))
-    self.fake("orders/450789469/risks/284138680", method='PUT', body= self.load_fixture('order_risk'), headers={'Content-type': 'application/json'})
+    self.fake("orders/450789469/risks/284138680", method='GET', body=self.load_fixture('order_risk'))
+    self.fake("orders/450789469/risks/284138680", method='PUT', body=self.load_fixture('order_risk'), headers={'Content-type': 'application/json'})
 
     v = shopify.OrderRisk.find(284138680, order_id=450789469)
     v.position = 3
