4fe3f27f44baff08df80786d500649c1ad00e2e4
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 10:19:45 -0700

    Fixed PYTHON_E251 issues in test/usage_charge_test.py

--- test/usage_charge_test.py
+++ test/usage_charge_test.py
@@ -12,5 +12,5 @@ class UsageChargeTest(TestCase):
     def test_get_usage_charge(self):
         self.fake("recurring_application_charges/654381177/usage_charges/359376002", method='GET', body=self.load_fixture('usage_charge'))
 
-        charge = shopify.UsageCharge.find(359376002, recurring_application_charge_id= 654381177)
+        charge = shopify.UsageCharge.find(359376002, recurring_application_charge_id=654381177)
         self.assertEqual('1000 emails', charge.description)
