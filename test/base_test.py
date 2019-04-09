f491b7845cf54a779b891ec63dab7c7f4e523d3c
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 10:16:15 -0700

    Fixed PYTHON_E225 issues in test/base_test.py

--- test/base_test.py
+++ test/base_test.py
@@ -69,7 +69,7 @@ class BaseTest(TestCase):
     def test_delete_should_send_custom_headers_with_request(self):
         shopify.ShopifyResource.activate_session(self.session1)
 
-        org_headers=shopify.ShopifyResource.headers
+        org_headers = shopify.ShopifyResource.headers
         shopify.ShopifyResource.set_headers({'X-Custom': 'abc'})
 
         with patch('shopify.ShopifyResource.connection.delete') as mock:
