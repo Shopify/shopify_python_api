98e889cdb227c84d80b5f6d25f293509b768ad5b
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2020-11-11 08:46:29 -0500

    Revert "Revert "[Feature] Add support for Shopify Payments resources""

---
+++ shopify/resources/balance.py
@@ -0,0 +1,7 @@
+from ..base import ShopifyResource
+from shopify import mixins
+
+
+class Balance(ShopifyResource, mixins.Metafields):
+    _prefix_source = "/shopify_payments/"
+    _singular = _plural = 'balance'
