a362f363010cc1b6cd11225a3cbdb90f47c03702
Paulina Khew <paulina.khew@shopify.com>
2021-01-28 16:54:07 -0500

    Clean up fulfillment_event_test.py

--- test/fulfillment_event_test.py
+++ test/fulfillment_event_test.py
@@ -3,20 +3,18 @@ from test.test_helper import TestCase
 from pyactiveresource.activeresource import ActiveResource
 
 class FulFillmentEventTest(TestCase):
-  def setUp(self):
-    super(FulFillmentEventTest, self).setUp()
-    self.fake("orders/450789469/fulfillments/255858046/events", method='GET', body=self.load_fixture('fulfillment_event'))
+    def setUp(self):
+        super(FulFillmentEventTest, self).setUp()
+        self.fake("orders/2776493818019/fulfillments/2608403447971/events", method='GET', body=self.load_fixture('fulfillment_event'))
+        self.fulfillment_event = shopify.FulfillmentEvent.find(order_id=2776493818019, fulfillment_id=2608403447971)
     
-  def test_get_fulfillment_event(self):
-    fulfillment_event = shopify.FulfillmentEvent.find(order_id='450789469', fulfillment_id='255858046')
-    self.assertEqual(1, len(fulfillment_event))
+    def test_get_fulfillment_event(self):
+        self.assertEqual(1, len(self.fulfillment_event))
 
-  # def test_create_fulfillment_event(self):
+    def test_create_fulfillment_event(self):
+        pass
 
-
-  def test_error_on_incorrect_status(self):
-    with self.assertRaises(AttributeError):
-      incorrect_status = 'asdf'
-      fulfillment_event = shopify.FulfillmentEvent.find(order_id='450789469', fulfillment_id='255858046')
-      fulfillment_event.status = incorrect_status
-      fulfillment_event.save()
+    def test_error_on_incorrect_status(self):
+        with self.assertRaises(AttributeError):
+            self.fake("orders/2776493818019/fulfillments/2608403447971/events", method='PUT', body=self.load_fixture('article'), headers={'Content-type': 'application/json'})
+            self.fulfillment_event.save()
