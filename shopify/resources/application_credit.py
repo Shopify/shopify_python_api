7fb299689f838e07e55e3a8cb866d15e52d2fedb
Paulina Khew <paulina.khew@shopify.com>
2021-02-09 17:24:10 -0500

    Replace logic in application_credit.py with a pass

--- shopify/resources/application_credit.py
+++ shopify/resources/application_credit.py
@@ -1,12 +1,4 @@
 from ..base import ShopifyResource
 
 class ApplicationCredit(ShopifyResource):
-    _prefix_source = "application_credits/$application_credit_id/"
-
-    @classmethod
-    def _prefix(cls, options={}):
-        application_credit_id = options.get("application_credit_id")
-        if application_credit_id:
-            return "%s/application_credits/%s" % (cls.site, application_credit_id)
-        else:
-            return "%s" % (cls.site)
+    pass
