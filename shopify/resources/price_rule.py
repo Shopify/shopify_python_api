e358d3f3206efa5b5f9ae10e3edb9dff3d8820c2
Paulina Khew <paulina.khew@shopify.com>
2021-02-17 15:06:49 -0500

    Run end-of-file-fixer to fix tests

--- shopify/resources/price_rule.py
+++ shopify/resources/price_rule.py
@@ -20,6 +20,3 @@ class PriceRule(ShopifyResource):
     def find_batch(self, batch_id):
         return DiscountCodeCreation.find_one("%s/price_rules/%s/batch/%s.%s" % (
             ShopifyResource.site, self.id, batch_id, PriceRule.format.extension))
-
-
-
