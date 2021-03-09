984849ab33b37e9c6318175e350c75f3cdd5c712
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-03-09 17:39:49 -0500

    Create sanitize_shop_domain utility method

---
+++ shopify/utils/shop_url.py
@@ -0,0 +1,20 @@
+import re
+import sys
+
+if sys.version_info[0] < 3:  # Backwards compatibility for python < v3.0.0
+    from urlparse import urlparse
+else:
+    from urllib.parse import urlparse
+
+HOSTNAME_PATTERN = r"[a-z0-9][a-z0-9-]*[a-z0-9]"
+
+
+def sanitize_shop_domain(shop_domain, myshopify_domain="myshopify.com"):
+    name = str(shop_domain).lower().strip()
+    if myshopify_domain not in name and "." not in name:
+        name += ".{domain}".format(domain=myshopify_domain)
+    name = re.sub(r"https?://", "", name)
+
+    uri = urlparse("http://{hostname}".format(hostname=name))
+    if re.match(r"{h}\.{d}$".format(h=HOSTNAME_PATTERN, d=re.escape(myshopify_domain)), uri.netloc):
+        return uri.netloc
