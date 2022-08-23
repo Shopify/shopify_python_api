7a9750e0d68d23ffb0c98c23779f56b79cb76b3a
Yashaskara Jois K R <43381463+Yashaskara@users.noreply.github.com>
2022-08-23 17:48:39 +0530

    Update product.py

--- shopify/resources/product.py
+++ shopify/resources/product.py
@@ -33,11 +33,4 @@ class Product(ShopifyResource, mixins.Metafields, mixins.Events):
     def save(self):
         start_api_version = "201910"
         api_version = ShopifyResource.version
-#         if api_version and (api_version.strip("-") >= start_api_version) and api_version != "unstable":
-#             if "variants" in self.attributes:
-#                 for variant in self.variants:
-#                     if "inventory_quantity" in variant.attributes:
-#                         del variant.attributes["inventory_quantity"]
-#                     if "old_inventory_quantity" in variant.attributes:
-#                         del variant.attributes["old_inventory_quantity"]
         return super(ShopifyResource, self).save()
